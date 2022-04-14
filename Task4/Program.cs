b940daa97eb2d67197704e9af9e51762a38bb774
Zakhar <zaharbaranov2003@mail.ru>
2022-04-14 21:03:03 +0700

    Четвёртое задание

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int number1, number2, number3, max;
+number1 = 10;
+number2 = 8;
+number3 = 60;
+max = number1;
+
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+}
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
