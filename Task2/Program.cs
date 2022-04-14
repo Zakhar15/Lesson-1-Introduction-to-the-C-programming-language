88b06e92bd2092bbe8df85512c906cdba0babc52
Zakhar <zaharbaranov2003@mail.ru>
2022-04-14 20:19:02 +0700

    Второе задание

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1, number2;
+number1 = 7;
+number2 = 11;
+Console.Write("Максимальное число: ");
+if (number1 > number2)
+    {
+        Console.WriteLine(number1);
+    }
+if (number1 < number2)
+    {
+        Console.WriteLine(number2);
+    }
