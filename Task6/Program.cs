40b05ac49d91288db4daba976bcb168a766bc0cf
Zakhar <zaharbaranov2003@mail.ru>
2022-04-17 09:03:25 +0700

    Шестое задание

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿int number;
+number = 15;
+if (number % 2 == 0)
+{
+    Console.Write("Четное число: ");
+    Console.WriteLine(number);
+}
+else
+{
+    Console.Write("Нечетное число: ");
+    Console.WriteLine(number);
+}
